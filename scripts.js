737ac752f391248e34a53aff7d617f9608da3456
Saad <saadrockywebsite@gmail.com>
2025-01-30 02:26:11 +0100

    youtube section update

--- scripts.js
+++ scripts.js
@@ -9,3 +9,23 @@ function showSection(sectionId) {
 
 // Show "About" section by default
 document.addEventListener("DOMContentLoaded", () => showSection('about'));
+
+
+function filterVideos() {
+    var input, filter, containers, title, i;
+    input = document.getElementById('videoSearch');
+    filter = input.value.toUpperCase();
+    containers = document.getElementsByClassName('video-container');
+
+    // Loop through all video containers
+    for (i = 0; i < containers.length; i++) {
+        title = containers[i].getAttribute('data-title');
+        
+        // Check if the title matches the search input
+        if (title.toUpperCase().indexOf(filter) > -1) {
+            containers[i].style.display = "";
+        } else {
+            containers[i].style.display = "none";
+        }
+    }
+}
